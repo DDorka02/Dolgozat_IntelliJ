38d0b8d58a0f1f6eba1acca4ab05dc0aed101f40
DDorka02 <dobszaydorka@gmail.com>
2024-12-09 09:34:41 +0100

    f1 4

--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -10,6 +10,33 @@ public class Tabla {
     }
 
 
+    public void feltolt(){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == 0 || (i + j == 4) || i < 5) {
+                    t[i][j] = '-';
+                } else {
+                    t[i][j] = ' ';
+                }
+            }
+        }
+    }
+
+
+    public void fajlbair(){
+
+    }
+
+    public void megjelenit(){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.println(t[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
 
 
 
